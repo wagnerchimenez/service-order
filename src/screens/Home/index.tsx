33875734f361cfed4dba0270adbbcbdc7bd750d4
Wagner Lima Chimenez <wagnerllchimenez.comp@gmail.com>
2025-04-01 13:47:22 -0400

    feat: Adicionado React Navigation

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+
+export function Home() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Home</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
