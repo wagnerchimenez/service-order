6b5c15af6ede30e6fcd800f8f29905b7ed03fee7
Wagner Lima Chimenez <wagnerllchimenez.comp@gmail.com>
2025-04-11 12:03:48 -0400

    feat: Implementado OrdersScreen

--- src/Order/Presentation/Screens/OrdersScreen/index.tsx
+++ src/Order/Presentation/Screens/OrdersScreen/index.tsx
@@ -1,6 +1,6 @@
 import { View, TextInput, Text, TouchableOpacity } from 'react-native'
-
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Icon from 'phosphor-react-native';
 
 import { Header } from "@/Shared/Infrastructure/Components/Header";
 
@@ -11,42 +11,60 @@ export function OrdersScreen() {
                 title="Ordens de Serviço"
             />
 
-            <View className="mb-2">
-                <TextInput
-                    placeholder="Pesquisar"
-                    className="border-2 border-gray-300 rounded-md p-2"
-                />
-            </View>
+            <View className="flex-1">
 
-
-
-            <View className="border-2 border-gray-300 rounded-md p-2">
-
-                <View className="flex-row justify-between">
-                    <Text className="text-lg font-bold">11/04/2025</Text>
-                    <TouchableOpacity className="bg-red-500 rounded-md size-6 justify-center items-center">
-                        <Text className="text-white">-</Text>
-                    </TouchableOpacity>
+                <View className="mb-2">
+                    <TextInput
+                        placeholder="Pesquisar"
+                        className="border-2 border-gray-300 rounded-md p-2"
+                    />
                 </View>
 
-                <View className="mt-2 gap-2">
-                    <Text className="text-lg font-bold">Wagner Lima Chimenez</Text>
+                <View className="border-2 border-gray-300 rounded-md p-2">
 
-                    <View className="flex-row gap-2">
-                        <Text className="font-bold">Total: </Text>
-                        <Text>R$ 100,00</Text>
+                    <View className="flex-row justify-between">
+                        <View>
+                            <Text className="text-lg font-bold">11/04/2025</Text>
+                        </View>
+                        <View className="flex-row gap-1">
+                            <TouchableOpacity className="bg-green-500 rounded-md size-6 justify-center items-center">
+                                <Text className="text-white">
+                                    <Icon.WhatsappLogo size={15} color="white" />
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="bg-blue-500 rounded-md size-6 justify-center items-center">
+                                <Text className="text-white">
+                                    <Icon.MagnifyingGlass size={15} color="white" />
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="bg-red-500 rounded-md size-6 justify-center items-center">
+                                <Text className="text-white">-</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
-                    <Text className="text-sm text-gray-500">Pendente</Text>
+                    <View className="mt-2 gap-2">
+                        <View className="flex-row gap-2">
+                            <Text className="text-lg font-bold">Cliente:</Text>
+                            <Text className="text-lg text-gray-900">Wagner Lima Chimenez</Text>
+                        </View>
+                        <View className="flex-row gap-2">
+                            <Text className="text-lg font-bold">Total: </Text>
+                            <Text className="text-lg text-gray-900">R$ 100,00</Text>
+                        </View>
+
+                        <Text className="text-sm text-gray-500">Pendente</Text>
+                    </View>
+
                 </View>
 
             </View>
 
-
-
-
-
-
+            <TouchableOpacity className="bg-green-500 rounded-md p-2">
+                <Text className="text-white text-center">
+                    Nova Ordem de Serviço
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
